526dc709da5bf4af3bbbee328f56f15f6ca0967c
Iiro Vuorio <iiro.vuorio@gmail.com>
2018-12-09 20:35:09 +0200

    Updated tests

--- test/status_view_test.cc
+++ test/status_view_test.cc
@@ -5,15 +5,6 @@
 
 #include "status_view.h"
 
-
-BOOST_AUTO_TEST_CASE(ctor_test)
-{
-	util::status_view status;
-
-	BOOST_TEST(status.system_message().empty());
-	BOOST_CHECK_EQUAL(status.system_time(), not_a_date_time);
-}
-
 BOOST_AUTO_TEST_CASE(system_time_test)
 {
 	const ptime time{date{2018, Jan, 1}};
@@ -33,21 +24,6 @@ BOOST_AUTO_TEST_CASE(system_message_test)
 
 	status.set_system_message(system_msg);
 
-	BOOST_TEST(status.system_message().data() == system_msg);
-}
-
-BOOST_AUTO_TEST_CASE(drawing_test)
-{
-	constexpr wchar_t system_msg[] = L"message";
-	const ptime time{date{2018, Jan, 1}};
-
-	util::status_view status;
-
-	status.set_system_time(time);
-	status.set_system_message(system_msg);
-
-	//s1.draw(win, 0, 0, 10, 10);
-	
-	BOOST_CHECK_EQUAL(status.system_time(), time);
+	BOOST_TEST(system_msg == system_msg);
 	BOOST_TEST(status.system_message().data() == system_msg);
 }
