95de3ed64a7af25558b0442be1f9ae8604ad4912
Iiro Vuorio <iiro.vuorio@gmail.com>
2018-12-10 04:57:58 +0200

    Fixed a merging error

--- src/utility.cc
+++ src/utility.cc
@@ -3,25 +3,19 @@
 #include <cstring>
 #include <stdexcept>
 
-std::vector<std::string>
-util::parse_value(const std::string& value)
+std::vector<std::pair<std::string, std::vector<std::string>>>
+util::parse_commandline(const int count, const char** vector)
 {
-	std::vector<std::string> temp;
-	std::size_t start = 0;
-	std::size_t size = 0;
-
-	do {
-		std::size_t end = value.find_first_of(',', start);
-		
-		if (end != std::string::npos)
-			size = end - start;
+	std::vector<std::pair<std::string, std::vector<std::string>>> temp;
+	for (unsigned i = 1; i < count; i++) {
+		const char* arg = vector[i];
+		if (std::strncmp(arg, "--", 2) == 0)
+			temp.emplace_back(std::pair(&arg[2],
+						    std::vector<std::string>{}));
+		else if (not temp.empty())
+			temp.back().second.emplace_back(arg);
 		else
-			size = std::string::npos;
-
-		temp.emplace_back(value.substr(start, size));
-
-		start = end + 1;
-	} while (size != std::string::npos);
-
+			throw std::logic_error{"found value without key"};
+	}
 	return temp;
 }
