e24b66cf9d7e7dc38094928ac1e71da1255ee6dd
Iiro Vuorio <iiro.vuorio@gmail.com>
2019-02-02 17:07:57 +0200

    Added a function for checking similarity of two strings

--- src/utility.cc
+++ src/utility.cc
@@ -19,3 +19,25 @@ util::parse_commandline(const int count, const char** vector)
 	}
 	return temp;
 }
+
+bool
+util::similar_str(const std::wstring_view& lhs,
+		  const std::wstring_view& rhs)
+{
+	const auto lhs_len = lhs.length();
+	const auto rhs_len = rhs.length();
+
+	if (lhs_len < rhs_len) {
+		if (rhs.find(lhs) != std::string_view::npos)
+			return (lhs_len*100)/rhs_len >= 33;
+		else
+			return false;
+	} else if (lhs_len > rhs_len) {
+		if (lhs.find(rhs) != std::string_view::npos)
+			return (rhs_len*100)/lhs_len >= 33;
+		else
+			return false;
+	} else {
+		return lhs == rhs;
+	}
+}
--- src/utility.h
+++ src/utility.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <string_view>
 #include <utility>
 #include <vector>
 
@@ -20,4 +21,16 @@ namespace util {
 	std::vector<std::pair<std::string,
 			      std::vector<std::string>>>
 	parse_commandline(const int count, const char** vector);
+
+	/* similar_str - compare two strings to see if they are similar
+	 * @lhs: string_view to the first string
+	 * @rhs: string_view to the second string
+	 *
+	 * Returns true if the two strings are similar. Similarity is checked by
+	 * seeing if the shorter string is a substring of the longer string and
+	 * if the substring length is atleast 33% of the length of the longer
+	 * string.
+	 */
+	bool similar_str(const std::wstring_view& lhs,
+			 const std::wstring_view& rhs);
 }
